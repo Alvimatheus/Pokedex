471a4c030b4a510a395ab338e9cea6ced959069e
Matheus Alvim <60630664+Alvimatheus@users.noreply.github.com>
2020-10-16 13:43:03 -0300

    header completed

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -13,10 +13,27 @@ export const Logo = styled.img`
     height: 200px;
 `;
 
+export const HolderContactLogo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ContactLogo = styled.img`
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+`;
+
 export default function AppHeader() {
     return (
         <Wrapper>
             <Logo src="/pokemon.png"></Logo>
+            <HolderContactLogo>
+                <ContactLogo src="/github-brands.svg"></ContactLogo>
+                <ContactLogo src="/twitter-square-brands.svg"></ContactLogo>
+                <ContactLogo src="/instagram-square-brands.svg"></ContactLogo>
+            </HolderContactLogo>
             <div>Created by Matheus Alvim</div>
         </Wrapper>
     )
